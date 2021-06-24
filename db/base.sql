1cc29786e973de22d12bc73d751d81e7a1138a0b
Karthik Raman <kartraman@gmail.com>
2021-06-24 17:31:19 +0530

    db update

--- db/base.sql
+++ db/base.sql
@@ -5,23 +5,42 @@ CREATE DATABASE IF NOT EXISTS `coredb`;
 GRANT ALL ON *.* TO 'root'@'%';
 
 
-DROP TABLE IF EXISTS `gl_account`;
-
--- DDL for accounting sub system related tables
-CREATE TABLE `gl_account` (
-  `id` BIGINT NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `parent_id` BIGINT DEFAULT NULL,
-  `gl_code` varchar(45) NOT NULL,
-  `disabled` tinyint NOT NULL DEFAULT '0',
-  `manual_journal_entries_allowed` tinyint NOT NULL DEFAULT '1',
-  `account_usage` tinyint NOT NULL DEFAULT '2',
-  `classification_enum` SMALLINT NOT NULL,
-  `description` varchar(500) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `acc_gl_code` (`gl_code`),
-  KEY `FK_ACC_0000000001` (`parent_id`),
-  CONSTRAINT `FK_ACC_0000000001` FOREIGN KEY (`parent_id`) REFERENCES `acc_gl_account` (`id`)
-) 
+DROP TABLE IF EXISTS gl_acct_jrnl_entry;
+DROP TABLE IF EXISTS gl_acct;
 
 
+CREATE table gl_acct (
+  id UUID DEFAULT uuid_generate_v4 (),
+  parent_id UUID DEFAULT NULL,
+  name VARCHAR(48) DEFAULT NULL,
+  gl_code VARCHAR(48) NOT NULL,
+  disabled BOOL,
+  description VARCHAR(256) DEFAULT NULL,
+  allow_manual_entries BOOL,
+  type VARCHAR(8) NOT NULL,
+  usage VARCHAR(8) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE (gl_code),
+  CONSTRAINT fk_ga_parent_id FOREIGN KEY (parent_id) REFERENCES gl_acct(id)
+);
+
+CREATE TABLE gl_acct_jrnl_entry (
+  id UUID DEFAULT uuid_generate_v4 (),
+  tranaction_id VARCHAR(50) NOT NULL,
+  ext_ref VARCHAR(50) DEFAULT NULL,
+  account_id UUID NOT NULL,
+  entry_date DATE NOT NULL,
+  amount DECIMAL(19,6) NOT NULL,
+  description VARCHAR(256),
+  reversal_id UUID DEFAULT NULL,
+  manual_entry BOOL,
+  entry_type INTEGER NOT NULL,
+  tsignature BYTEA NOT NULL,  
+  PRIMARY KEY (id),
+  CONSTRAINT fk_gaje_account_id FOREIGN KEY (account_id) REFERENCES gl_acct(id),
+  CONSTRAINT fk_gaje_reversal_id FOREIGN KEY (reversal_id) REFERENCES gl_acct_jrnl_entry(id)
+);
+
+CREATE INDEX idx_gaje_tranaction_id ON gl_acct_jrnl_entry(tranaction_id);
+CREATE INDEX idx_gaje_account_id ON gl_acct_jrnl_entry(account_id);
+CREATE INDEX idx_gaje_entry_date ON gl_acct_jrnl_entry(entry_date);
